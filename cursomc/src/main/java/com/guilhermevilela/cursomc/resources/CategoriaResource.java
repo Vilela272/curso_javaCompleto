391efab176b86c2ed104838531a40b38690d5186
Guilherme Vilela <guilherme.moreira@aluno.faculdadeimpacta.com.br>
2020-06-08 22:27:11 -0300

    Testando a primeica classe de dominio - Categoria

--- cursomc/src/main/java/com/guilhermevilela/cursomc/resources/CategoriaResource.java
+++ cursomc/src/main/java/com/guilhermevilela/cursomc/resources/CategoriaResource.java
@@ -1,12 +1,17 @@
 package com.guilhermevilela.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.guilhermevilela.cursomc.domain.Categoria;
+
 /*
- * @author: Guilherme Vilela
- * @version: 1.0.0
+ * @author Guilherme Vilela
+ * @version 1.0.0
  */
 
 /*
@@ -22,8 +27,16 @@ public class CategoriaResource {
 	 */
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria categoria1 = new Categoria(1, "informática");
+		Categoria categoria2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(categoria1);
+		lista.add(categoria2);
+		
+		return lista;
 	}
 
 }
